8374add1ab85febf88dd07ce9b2ad4c15798cd64
iuliia_sut <juliasutygina@gmail.com>
2021-06-23 18:56:31 -0600

    Added comment section explaining the function

--- eqArrays.js
+++ eqArrays.js
@@ -9,19 +9,22 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(arr1, arr2) {
-  return arr1.length === arr2.length &&
-  arr1.every((val, index) => val === arr2[index]);
+   if (arr1.length !== arr2.length) {
+     return false;
+   }
+   return arr1.every((val, index) => val === arr2[index]);
 };
+
+//checking the length of the arrays in case last elements of one left unevaluated
+
+
+// .every() method goes though each element of an array;
+// passes a callback function;
+// and passes each element to that function;
+// checks something and returns true or false;
+// only if each(!) element returns true, .each() method returns a true;
+
+
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1], [1, 2, 3]), true);
-
-/* const eqArrays2 =function(a, b) {
-  a.forEach((num1, index) => {
-    let num2 = b[index];
-    return num1 === num2;
-  })
-};
-
-console.log(arrayEquals([1, 4, 3], [1, 4, 3])); //undefined
-*/

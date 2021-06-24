0461cbecdaaa6d6f022979496d0ed9004e18b0ae
iuliia_sut <juliasutygina@gmail.com>
2021-06-23 18:59:23 -0600

    Implemented assertArraysEqual function, using eqArrays function that console logs assertion message

---
+++ assertArraysEqual.js
@@ -0,0 +1,18 @@
+const eqArrays = function(arr1, arr2) {
+  return arr1.length === arr2.length &&
+  arr1.every((val, index) => val === arr2[index]);
+};
+
+
+const assertArrayEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅ ✅ ✅  Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+assertArrayEqual([1, 3, 5], [1, 3, 5]);
+assertArrayEqual(["Ok", 3, 5], [1, 3, 5]);
+assertArrayEqual([1, 5], [1, 3, 5]);
+assertArrayEqual([], []);

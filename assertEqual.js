329e319b181fc75299cb4f82bc69f8f47eef8a3f
iuliia_sut <juliasutygina@gmail.com>
2021-06-24 19:19:34 -0600

    Updated assertEqual function

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,21 @@
+/* initial version
+
 const assertEqual = function(actual, expected) {
   if (String(actual) !== String(expected)) {
     console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
   } else {
     console.log(`✅ ✅ ✅  Assertion Passed: ${actual} === ${expected}`);
   }
+}; */
+
+const assertEqual = function(actual, expected) {
+  let answer = `❌ Assertion Failed: ${actual} !== ${expected}`;
+  if (String(actual) === String(expected)) {
+    answer = `✅ ✅ ✅  Assertion Passed: ${actual} === ${expected}`;
+  }
+  console.log(answer);
 };
+
 
 // TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");

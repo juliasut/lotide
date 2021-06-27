3cef86881f85a90987d3aa7aaf2509a74a6db988
iuliia_sut <juliasutygina@gmail.com>
2021-06-27 04:05:42 -0600

    Add another solution of head function

--- head.js
+++ head.js
@@ -1,19 +1,15 @@
 const assertEqual = function(actual, expected) {
   if (String(actual) === String(expected)) {
-    console.log(`✅ ✅ ✅  Assertion Passed: ${actual} === ${expected}`);
-    
+    console.log(`✅ ✅ ✅  Assertion Passed: ${actual} === ${expected}`); 
   } else {
     console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-    
   }
 };
 
 const head = function(arr) {
-  const [first, ...other] = arr;
+  const [first, ...other] = arr;  // other version: const head = arr => arr[0]
   return first;
 };
-
-
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");

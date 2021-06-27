37e1c41fd0148ffb42bfdffb7ef11a54de5faf40
iuliia_sut <juliasutygina@gmail.com>
2021-06-27 04:01:58 -0600

    Add comments explaining the logic

--- letterPositions.js
+++ letterPositions.js
@@ -11,19 +11,18 @@ const assertArraysEqual = function(arr1, arr2) {
 };
 
 
-
 const letterPositions = function(sentence) {
   const results = {};
   
   for (let i = 0; i < sentence.length; i++) {
-    if (results[sentence[i]]) {
+    if (results[sentence[i]]) {          //if already added to the array of occurences, add next one too
       results[sentence[i]].push(i)
-    } else if (sentence[i] !== " ") {
+    } else if (sentence[i] !== " ") {   //if not a white space, add to the array
         results[sentence[i]] = [i]
     }
   }
-
   return results;
 };
+
 assertArraysEqual(letterPositions("hello").e, [1]);
 console.log(letterPositions("Lighthouse in the house"));

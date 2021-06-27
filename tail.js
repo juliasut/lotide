fd0a044a6993e91be402aecce1695d30f5b4cde0
iuliia_sut <juliasutygina@gmail.com>
2021-06-27 03:06:24 -0600

    Fix deconstruction of the array, add test case

--- tail.js
+++ tail.js
@@ -9,9 +9,10 @@ const assertEqual = function(actual, expected) {
 };
 
 const tail = function(arr) {
-  const [...other] = arr;
+  const [first, ...other] = arr;
   return other;
 };
 const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
+
 assertEqual(words.length, 3);
+assertEqual(tail(words).length, 2);

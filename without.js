2a54e705b366afe00b2239a4d09c2d9c593c98c6
iuliia_sut <juliasutygina@gmail.com>
2021-06-27 02:18:47 -0600

    Added another version of without function in the comments

--- without.js
+++ without.js
@@ -21,6 +21,18 @@ const without = function(source, itemsToRemove) {
   return arrayWithout;
 };
 
+// const without = function(source, itemsToRemove) {
+//   let arrayWithout = [...source]; //make a copy of the original array to modify
+//   for (let i = 0; i < arrayWithout.length; i++) {
+//     for (let itemToRemove of itemsToRemove) {
+//       if (arrayWithout[i] === itemToRemove) {
+//         arrayWithout.splice(i, 1);
+//       }
+//     }
+//   }
+//   return arrayWithout;
+// }
+
 assertArrayEqual(without([1, 2, 3], [1]), [2, 3]);
 assertArrayEqual(without(["k", "2", 3], ["k", 2, 3]), ["2"]);
 assertArrayEqual(without(["k", "2", 3], ["k", 2, 3]), [2]);

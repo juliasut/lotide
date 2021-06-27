1ea702502eb718bece989092499619c00b2540ea
iuliia_sut <juliasutygina@gmail.com>
2021-06-27 12:02:49 -0600

    Add more tests, fix logic of eqObjects function

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,20 +1,29 @@
 const eqObjects = function(obj1, obj2) {
-  if (Object.keys(obj1).length === Object.keys(obj2).length) { 
-    for (const val in obj1) { 
-      return obj1[val] === obj2[val];
+  const obj1KeysArray = Object.keys(obj1);
+  const obj2KeysArray = Object.keys(obj2);
+
+  if (obj1KeysArray.length !== obj2KeysArray.length) {
+    return false;
     }
-  }
-  return false;
+
+    for (const key1 in obj1) { 
+      if (obj1[key1] !== obj2[key1]) {
+        return false;
+      } 
+    }
+  return true;
 };
 
 const assertObjectsEqual = function(obj1, obj2) {
   const inspect = require('util').inspect;
-  let answer = `❌ Assertion Failed: ${inspect(obj1)} !== ${inspect(obj2)}`;
 
   if (eqObjects(obj1, obj2)) {
-    answer = `✅ ✅ ✅  Assertion Passed: ${inspect(obj1)} === ${inspect(obj2)}`;
+    console.log(`✅ ✅ ✅  Assertion Passed: ${inspect(obj1)} === ${inspect(obj2)}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${inspect(obj1)} !== ${inspect(obj2)}`);
   }
-  console.log(answer);
-};
+}
 
-assertObjectsEqual({a: '1', b: 2}, {b: 2, a: '1'}, true);
+assertObjectsEqual({a: 'one', b: 2}, {b: 2, a: 'one'}); //passed
+assertObjectsEqual({a: '5', b: 2}, {b: 2, a: '1'}); //failed
+assertObjectsEqual({a: '1', b: "string"}, {b: 2, a: '1'}); //failed

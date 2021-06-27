b6b81cfc26e147ca9f246d334137d20453748d08
iuliia_sut <juliasutygina@gmail.com>
2021-06-27 05:28:29 -0600

    Uncomment the tests

--- findKey.js
+++ findKey.js
@@ -20,7 +20,7 @@ const findKey = function(planetSystem, callback) {
       return param;
     }
   }
-}
+} */
 
 findKey({
   "Blue Hill": { stars: 1 },
@@ -40,27 +40,26 @@ findKey({
   "Akelarre":  { stars: 3 }
 }, x => x.stars === 3) // => "Akaleri"
 
-// assertEqual(findKey({
-//   "Blue Hill": { stars: 1 },
-//   "Akaleri":   { stars: 3 },
-//   "noma":      { stars: 2 },
-//   "elBulli":   { stars: 3 },
-//   "Ora":       { stars: 2 },
-//   "Akelarre":  { stars: 3 }
-// }, x => x.stars === 2), "noma");
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma");
 
-// assertEqual(findKey({
-//   "Blue Hill": { stars: 1 },
-//   "elBulli":   { stars: 3 },
-//   "Ora":       { stars: 2 },
-//   "Akelarre":  { stars: 3 }
-// }, x => x.stars === 2), "noma");
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma");
 
-// assertEqual(findKey({
-//   "elBulli":   { stars: 3 },
-//   "Ora":       { stars: 2 },
-//   "Akelarre":  { stars: 3 }
-// }, x => x.stars === 2), "Ora");
+assertEqual(findKey({
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "Ora");
 
-// assertEqual(findKey({
-// }, x => x.stars === 2), undefined);
+assertEqual(findKey({}, x => x.stars === 2), undefined)

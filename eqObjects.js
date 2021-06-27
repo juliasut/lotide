f46f2f1c9d792f698469658c973ddeb9b9b5a989
iuliia_sut <juliasutygina@gmail.com>
2021-06-27 10:02:20 -0600

    Fix logic of eqObjects function, use inspect() in assertEqual

--- eqObjects.js
+++ eqObjects.js
@@ -1,19 +1,27 @@
-const assertEqual = function(actual, expected) {
-  let answer = `❌ Assertion Failed: ${actual} !== ${expected}`;
-  if (String(actual) === String(expected)) {
-    answer = `✅ ✅ ✅  Assertion Passed: ${actual} === ${expected}`;
+const assertEqual = function(obj1, obj2) {
+  const inspect = require('util').inspect;
+  let answer = `❌ Assertion Failed: ${inspect(obj1)} !== ${inspect(obj2)}`;
+  if (String(obj1) === String(obj2)) {
+    answer = `✅ ✅ ✅  Assertion Passed: ${inspect(obj1)} === ${inspect(obj2)}`;
   }
-  console.log(answer);
+  return answer;
 };
 
-const eqObjects = function(obj1, obj2) {
 
-  if (Object.keys(obj1).length === Object.keys(obj2).length) { // 1. does object1 and object2 have the same number of properties?
-    for (const val in obj1) { // 2. scan properties in one object1;
-      return obj1[val] === obj2[val]; // 3. check if the value in object1 equals to the value for that same property in object2; order doesn't matter here 
+const eqObjects = function(obj1, obj2) {
+  const obj1KeysArray = Object.keys(obj1);
+  const obj2KeysArray = Object.keys(obj2);
+
+  if (obj1KeysArray.length !== obj2KeysArray.length) {
+    return false;
     }
-  }
-  return false;
+
+    for (const key1 in obj1) { 
+      if (obj1[key1] !== obj2[key1]) {
+        return false;
+      } 
+    }
+  return true;
 };
 
 const ab = { a: "1", b: "2"};
@@ -24,3 +32,7 @@ assertEqual(eqObjects(ab, ba), true); // assertion passed
 const abc = { a: "1", b: "2", c: "3"};
 eqObjects(ab, abc); // false
 assertEqual(eqObjects(ab, abc), false); // assertion passed
+
+const ac = { a: "1", c: "m"};
+eqObjects(ab, ac); // false
+assertEqual(eqObjects(ab, ac), false) // assertion passed

17b53e8ef57d6769d50640ad7563d91bc124e251
iuliia_sut <juliasutygina@gmail.com>
2021-06-27 10:54:07 -0600

    Fix a bug in assertEqual function using JSON.stringify()

--- countLetters.js
+++ countLetters.js
@@ -1,6 +1,7 @@
 const assertEqual = function(actual, expected) {
+  
   let answer = `❌ Assertion Failed: ${actual} !== ${expected}`;
-  if (String(actual) === String(expected)) {
+  if (JSON.stringify(actual) === JSON.stringify(expected)) {
     answer = `✅ ✅ ✅  Assertion Passed: ${actual} === ${expected}`;
   }
   console.log(answer);
@@ -8,7 +9,6 @@ const assertEqual = function(actual, expected) {
 
 const countLetters = function(string) {
   let result = {};
-
   for (const letter of string.toLowerCase()) {
     if (result[letter]) {
       result[letter] += 1;
@@ -19,4 +19,7 @@ const countLetters = function(string) {
   return result;
 };
 
-assertEqual(countLetters("Light in"), {l: 1, i: 2, g: 1, h: 1, t: 1, n: 1});
+assertEqual(countLetters("Light in"), { l: 4, i: 2, g: 1, h: 1, t: 1, n: 1 });
+assertEqual(countLetters("Light in"), { l: 1, i: 2, g: 1, h: 1, t: 1, n: 1 });
+assertEqual(countLetters("1Hello"), { e: 1, l: 2, o: 1 });
+assertEqual(countLetters("A D a"), { a: 2, d: 1 });

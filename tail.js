e8579aa8f0c5553320ac74ec0d64a24da1f50079
iuliia_sut <juliasutygina@gmail.com>
2021-06-29 05:51:11 -0600

    Export and require tail and assertEqual function

--- tail.js
+++ tail.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (String(actual) === String(expected)) {
-    console.log(`✅ ✅ ✅  Assertion Passed: ${actual} === ${expected}`);
-    
-  } else {
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
 const tail = function(arr) {
   const [first, ...other] = arr;
@@ -14,7 +6,4 @@ const tail = function(arr) {
 
 //const tail = arr => return arr.slice(1); - return an arr copy starting at position 1
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-
-assertEqual(words.length, 3);
-assertEqual(tail(words).length, 2);
+module.exports = tail;

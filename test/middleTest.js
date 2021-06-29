6975f96a9a0bcfc6aecfb1ec711c2d5c1531b9d0
iuliia_sut <juliasutygina@gmail.com>
2021-06-29 07:01:08 -0600

    Refactor test with Mocha and Chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,11 +1,29 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
+describe("#middle", () => {
+  it("returns empty array if array has one element", () => {
+    assert.deepEqual(middle([3]), []);
+  });
 
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+  it("returns empty array if array has less that three elements", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  it("returns a single middle element if array has odd number of elements", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+
+  it("returns two middle elements if array has even number of elements", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+
+});
+
+
+
+
+
+
+// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);

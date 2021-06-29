fb091963173e9408ada76c49f67adb35285b7188
iuliia_sut <juliasutygina@gmail.com>
2021-06-29 05:33:22 -0600

    Import and require functions

--- head.js
+++ head.js
@@ -1,18 +1,8 @@
-const assertEqual = function(actual, expected) {
-  if (String(actual) === String(expected)) {
-    console.log(`✅ ✅ ✅  Assertion Passed: ${actual} === ${expected}`); 
-  } else {
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(arr) {
   const [first, ...other] = arr;  // other version: const head = arr => arr[0]
   return first;
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), "Other");
-assertEqual(head([]), "1");
-assertEqual(head(["one"]), "one");
+module.exports = head;
---
+++ test/assertEqualTest.js
@@ -0,0 +1,7 @@
+const assertEqual = require('../assertEqual');
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("45", "45");
+assertEqual(1, 1);
+assertEqual(3, 10);
